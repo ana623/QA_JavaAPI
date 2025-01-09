1b54bdf82db18c5f8585b0ecf64276b05fbd5a9a
ana623 <a.batakovskaya@gmail.com>
2025-01-10 00:30:49 +0300

    10,11,12 added

---
+++ src/test/java/archive/Ex10StringLengthTest.java
@@ -0,0 +1,18 @@
+package archive;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Ex10StringLengthTest {
+
+    @Test
+    /* В рамках этой задачи с помощью JUnit необходимо написать тест, который проверяет длину переменной типа String
+     с помощью любого выбранного метода assert.
+    Если текст длиннее 15 символов, то тест должен проходить успешно. Иначе падать с ошибкой.*/
+    public void testStringLength() {
+        String testString = "length < 15";
+        assertTrue(testString.length() > 15, "Unexpected string length");
+    }
+}
